e187f69e628ca2d091876831d279775bc53995c0
Yugy <me@yanghui.name>
2015-09-19 17:09:29 +0800

    repo init

---
+++ demo/src/main/java/me/yugy/github/screenshotlayout/MainActivity.java
@@ -0,0 +1,26 @@
+package me.yugy.github.screenshotlayout;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import me.yugy.github.library.screenshotlayout.ScreenShotLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ScreenShotLayout.attachToActivity(this);
+
+        ListView list = (ListView) findViewById(R.id.list);
+        list.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new String[]{
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+            "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+        }));
+    }
+}
